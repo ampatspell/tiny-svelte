297c4aa585e3c6f72f033e9824112080e9889c4f
ampatspell <ampatspell@gmail.com>
2024-02-09 17:43:53 +0200

    add use:resize

---
+++ src/lib/canvas/canvas.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Size } from "$lib/types";
+	import { resize } from "$lib/use-resize.svelte";
+
+  let onResize = (size: Size) => {
+    console.log(size);
+  }
+
+</script>
+
+<canvas class="canvas" use:resize={{ onResize }}/>
+
+<style lang="scss">
+  .canvas {
+    background: rgba(255,0,0,0.1);
+    width: 100%;
+    height: 100%;
+  }
+</style>
---
+++ src/routes/dev/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Canvas from "$lib/canvas/canvas.svelte";
+</script>
+
+<div class="page">
+  <div class="container">
+    <Canvas/>
+  </div>
+</div>
+
+<style lang="scss">
+  .page {
+    flex: 1;
+    padding: 15px;
+    > .container {
+      width: 100%;
+      height: 100%;
+      outline: 1px solid rgba(255,0,0,0.3);
+    }
+  }
+</style>
