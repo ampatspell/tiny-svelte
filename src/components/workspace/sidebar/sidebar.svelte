d9ad325ea6be2e3b9e226ef72d1b12ec9319a204
ampatspell <ampatspell@gmail.com>
2024-03-17 15:32:59 +0200

    move sidebar to another component

---
+++ src/components/workspace/sidebar/sidebar.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import type { ProjectModel } from '$lib/models/project/project.svelte';
+  import type { WorkspaceModel } from '$lib/models/project/workspace/workspace.svelte';
+  import Assets from './assets.svelte';
+  import Nodes from './nodes.svelte';
+  import Workspaces from './workspaces.svelte';
+
+  let {
+    project,
+    workspace
+  }: {
+    project: ProjectModel;
+    workspace: WorkspaceModel;
+  } = $props();
+
+  let workspaces = $derived(project.workspaces);
+</script>
+
+<div class="sidebar">
+  <div class="section">
+    <div class="title">Workspaces</div>
+    <Workspaces {workspaces} selected={workspace} />
+  </div>
+  <div class="section">
+    <div class="title">Assets</div>
+    <Assets assets={workspace.assets} />
+  </div>
+  <div class="section">
+    <div class="title">Nodes</div>
+    <Nodes nodes={workspace.nodes} />
+  </div>
+</div>
+
+<style lang="scss">
+  .sidebar {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    > .section {
+      > .title {
+        padding: 10px;
+        font-weight: 600;
+        border-bottom: 1px solid fade-out(#000, 0.95);
+      }
+    }
+  }
+</style>
--- src/routes/editor/projects/[project]/[workspace]/+page.svelte
+++ src/routes/editor/projects/[project]/[workspace]/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
   import Loadable from '$components/basic/loadable.svelte';
-  import Assets from '$components/workspace/sidebar/assets.svelte';
-  import Nodes from '$components/workspace/sidebar/nodes.svelte';
-  import Workspaces from '$components/workspace/sidebar/workspaces.svelte';
+  import Sidebar from '$components/workspace/sidebar/sidebar.svelte';
   import { activate } from '$lib/firebase/firestore.svelte';
 
   let { data } = $props();
@@ -11,24 +9,12 @@
   $effect(() => activate(workspace));
 
   let project = $derived(data.workspace.project);
-  let workspaces = $derived(project.workspaces);
 </script>
 
 <Loadable model={workspace}>
   <div class="page">
     <div class="left">
-      <div class="section">
-        <div class="title">Workspaces</div>
-        <Workspaces {workspaces} selected={workspace} />
-      </div>
-      <div class="section">
-        <div class="title">Assets</div>
-        <Assets assets={workspace.assets} />
-      </div>
-      <div class="section">
-        <div class="title">Nodes</div>
-        <Nodes nodes={workspace.nodes} />
-      </div>
+      <Sidebar {project} {workspace} />
     </div>
   </div>
 </Loadable>
@@ -41,16 +27,6 @@
     > .left {
       width: 250px;
       border-right: 1px solid fade-out(#000, 0.95);
-      display: flex;
-      flex-direction: column;
-      gap: 20px;
-      > .section {
-        > .title {
-          padding: 10px;
-          font-weight: 600;
-          border-bottom: 1px solid fade-out(#000, 0.95);
-        }
-      }
     }
     > .content {
       flex: 1;
