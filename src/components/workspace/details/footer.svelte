bd70b5ce256b01e0b0a2d2b898d96d60111550c3
ampatspell <ampatspell@gmail.com>
2024-03-18 21:53:19 +0200

    Update footer.svelte

--- src/components/workspace/details/footer.svelte
+++ src/components/workspace/details/footer.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-  import Inspector from '$components/basic/inspector/inspector.svelte';
   import Pixel from '$components/basic/inspector/item/pixel.svelte';
-  import Section from '$components/basic/inspector/section.svelte';
   import type { WorkspaceModel } from '$lib/models/project/workspace/workspace.svelte';
 
   let {
@@ -11,8 +9,4 @@
   } = $props();
 </script>
 
-<Inspector>
-  <Section>
-    <Pixel omitTitle={true} model={workspace} />
-  </Section>
-</Inspector>
+<Pixel omitTitle={true} model={workspace} />
