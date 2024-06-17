c020d467f4380bafb337f5a6f7677a4f99f95729
ampatspell <ampatspell@gmail.com>
2024-06-17 20:53:02 +0300

    update deps

--- src/routes/editor/projects/[project]/[workspace]/+page.svelte
+++ src/routes/editor/projects/[project]/[workspace]/+page.svelte
@@ -1,22 +1,10 @@
 <script lang="ts">
   import Workspace from '$features/editor/workspace/workspace.svelte';
   import { subscribe } from '$lib/firebase/fire/subscriber.svelte.js';
-  // import { ProjectModel } from '$lib/models/project/project.svelte.js';
-  // import { WorkspaceModel } from '$lib/models/project/workspace/workspace.svelte.js';
-  // import { getter } from '$lib/utils/args.js';
 
   let { data } = $props();
   let workspace = $derived(data.workspace);
   $effect(() => subscribe(workspace));
-
-  // let workspace = new WorkspaceModel({
-  //   project: new ProjectModel({
-  //     id: getter(() => data.project.id),
-  //   }),
-  //   id: getter(() => data.workspace.id),
-  // });
-
-  // subscribe(workspace);
 </script>
 
 <Workspace {workspace} />
